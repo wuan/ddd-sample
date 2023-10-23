cc48a28cdffcb79aa633ff15a7b04426990e6231
Niels Cölle <niels.colle@thoughtworks.com>
2023-10-23 11:53:21 +0200

    Adapt main with current tests

--- src/main/java/com/ddd_bootcamp/application/Application.kt
+++ src/main/java/com/ddd_bootcamp/application/Application.kt
@@ -1,12 +1,15 @@
 package com.ddd_bootcamp.application
 
 import com.ddd_bootcamp.domain.Cart
+import com.ddd_bootcamp.domain.CartItem
 import com.ddd_bootcamp.domain.Product
 
 fun main(args: Array<String>) {
     val cart = Cart()
-    val product = Product("Apple Pencil")
-    cart.add(product)
+    val pencil = Product("Apple Pencil")
+    cart.add(pencil)
+    val sonies = CartItem(Product("Sony Wireless Headphone"), 2)
+    cart.add(sonies)
     println("Cart = $cart")
     val products = cart.getItems()
     println("----------------------------------------")
