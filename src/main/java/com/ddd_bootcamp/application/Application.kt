45e708d0524dc7cc939cb46bacb532e5c185670c
Niels Cölle <niels.colle@thoughtworks.com>
2023-10-23 12:43:46 +0200

    Fix error in call Cart.add

--- src/main/java/com/ddd_bootcamp/application/Application.kt
+++ src/main/java/com/ddd_bootcamp/application/Application.kt
@@ -1,15 +1,14 @@
 package com.ddd_bootcamp.application
 
 import com.ddd_bootcamp.domain.Cart
-import com.ddd_bootcamp.domain.CartItem
 import com.ddd_bootcamp.domain.Product
 
 fun main(args: Array<String>) {
     val cart = Cart()
     val pencil = Product("Apple Pencil")
     cart.add(pencil)
-    val sonies = CartItem(Product("Sony Wireless Headphone"), 2)
-    cart.add(sonies)
+    val sonies = Product("Sony Wireless Headphone")
+    cart.add(sonies, 2)
     println("Cart = $cart")
     val products = cart.getItems()
     println("----------------------------------------")
