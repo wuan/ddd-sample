539f06c1862803aad0dc846f8008f53ebb525036
Niels Cölle <niels.colle@thoughtworks.com>
2023-10-23 11:33:07 +0200

    Add test for more products

--- src/test/java/com/ddd_bootcamp/domain/CartTest.kt
+++ src/test/java/com/ddd_bootcamp/domain/CartTest.kt
@@ -16,4 +16,17 @@ internal class CartTest {
         assertThat(actual).hasSize(1)
         assertThat(actual[0].name).isEqualTo("Some test product", actual[0].name)
     }
+
+    @Test
+    fun shouldAdd2ProductsToCart() {
+        val cart = Cart()
+        val product = Product("Some test product")
+        val sony = Product("Sony Wireless Headphone")
+        cart.add(product)
+        cart.add(sony)
+        val actual = cart.getProducts()
+
+        assertThat(actual).hasSize(2)
+        assertThat(actual).extracting("name").contains("Some test product", "Sony Wireless Headphone")
+    }
 }
